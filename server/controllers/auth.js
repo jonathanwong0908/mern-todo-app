2972a737019229e9f73f33597c08de6353cd19d3
Jonathan <ghuyers98@gmail.com>
2023-01-13 15:07:22 +0800

    set up models

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,1 +1,11 @@
-const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+export const registerUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
